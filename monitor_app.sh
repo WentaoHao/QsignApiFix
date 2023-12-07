d76ffa8f251b0691607cc8e4773db1fa2adaae54
hao_wen_tao666 <iamhaowentao@qq.com>
2023-12-07 16:25:41 +0800

    每小时自动重启一次，防止Java进程占用过多内存

--- monitor_app.sh
+++ monitor_app.sh
@@ -9,7 +9,7 @@ else
         
 fi
 
-#改成自己的目录
+#改成自己QSign的目录
 dir="/home/haowentao/Miao-Yunzai/Qsign/unidbg-fetch-qsign"
 #改成自己的版本
 version="8.9.93"
@@ -24,18 +24,33 @@ if pgrep -f java ; then
     kill -9 $pid
 fi
 
-# app="sudo bash $bindir $basePathDir $output"
-# echo "$app"
-# 提权为root
-# sudo -v
+# 记录本次启动的时间点
+firstTime=$(date +"%s")
 # 启动API，将输出重定向到临时文件
-# bash $dir/bin/unidbg-fetch-qsign --basePath=txlib/8.9.93  > output.log 2>&1
 bash $bindir $basePathDir > $output 2>&1 &
 while true; do
-    
-    # 检查输出文件中是否包含关键字
-    if grep -q '警告' output.log; then
-        echo "Detected warning or error. Restarting the app..."
+    # 计算本次运行的时长
+    secondTime=$(date +"%s")
+    duration=$((secondTime - firstTime))
+
+    # TODO:
+    # 计算两次重启的时间差值，小于10分钟则重启喵崽
+    # 使用 pgrep -f 找到所有匹配的 PID，并使用 pkill 杀死它们
+
+    # pids=$(pgrep -f "your_process_name_or_pattern")
+
+    # if [ -n "$pids" ]; then
+    #     echo "Killing processes with PIDs: $pids"
+    #     pkill -P "$pids"
+    # 
+    # fi
+
+
+    # 检查输出文件中是否包含关键字，或运行时长超过1小时
+    if grep -q '警告' output.log || ((duration > 3600)) ; then
+        
+        echo "Detected warning or overtime. Restarting the API..."
+        
         
         # 获取java的进程ID并杀死它
         pid=$(pgrep -f "java")
@@ -43,6 +58,8 @@ while true; do
         kill -9 $pid
         # 删除日志文件
         rm -f output.log
+        #记录本次启动的时间点
+        firstTime=$(date +"%s")
         # 等待一段时间，然后重新启动API
         sleep 5
         bash $bindir $basePathDir > $output 2>&1 &
@@ -55,7 +72,6 @@ while true; do
         else
             echo "Java is not running. Reruning API..."
             bash $bindir $basePathDir > $output 2>&1 &
-        fi
-        
+        fi  
     fi
 done
